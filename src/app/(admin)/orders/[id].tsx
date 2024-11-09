1367d5c3bdfc01ed17b8de3a16dac3a0e67a883c
oscarivanrs <oscarivan.sanchez@gmail.com>
2024-11-09 11:35:14 +0000

    chore: orders status selector

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -2,11 +2,19 @@ import OrderItemListItem from "@/components/OrderItemListItem";
 import OrderListItem from "@/components/OrderListItem";
 import orders from '@assets/data/orders';
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, StyleSheet, FlatList, Text } from "react-native"
+import { View, StyleSheet, FlatList, Text, Pressable } from "react-native"
+import { OrderStatusList, OrderStatus } from "@/types";
+import { Colors } from "@/constants/Colors";
+import { useState } from "react";
 
 export default function orderDetails() {
     const { id } = useLocalSearchParams();
     const order = orders.find((o) => o.id.toString() === id);
+    const [orederStatus, setOrderStatus] = useState<OrderStatus>('New');
+
+    const updateStatus = async (stato: string) => {
+        console.log(`updateStatus(${stato})`)
+      };
 
     if(!order) {
         return (
@@ -18,14 +26,50 @@ export default function orderDetails() {
     }
 
     return (
-        <View style={styles.container}>
-            <Stack.Screen options={{ title: `Order #${order.id}` }} />
-            <OrderListItem order={order} />
-            <FlatList data={order.order_items} 
-            renderItem={({ item }) => <OrderItemListItem orderItem={item} />}
-            contentContainerStyle={{ gap: 10 }}
-            />
-        </View>
+      <View style={styles.container}>
+        <Stack.Screen options={{ title: `Order #${order.id}` }} />
+        <OrderListItem order={order} />
+        <FlatList
+          data={order.order_items}
+          renderItem={({ item }) => <OrderItemListItem orderItem={item} />}
+          contentContainerStyle={{ gap: 10 }}
+          ListFooterComponent={() => (
+            <>
+              <Text>Status</Text>
+              <View style={styles.ordersStatus}>
+                {OrderStatusList.map((stato) => (
+                  <Pressable
+                    key={stato}
+                    onPress={() => updateStatus(stato)}
+                    style={[styles.orderStatus,
+                        {
+                            backgroundColor:
+                            order.status === stato
+                            ? Colors.light.tint
+                            : 'transparent'
+                        }
+                    ]}
+                  >
+                    <Text
+                      style={[
+                        styles.orderStatusText,
+                        {
+                          color:
+                            order.status === stato
+                              ? "white"
+                              : Colors.light.tint,
+                        },
+                      ]}
+                    >
+                      {stato}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+            </>
+          )}
+        />
+      </View>
     );
 }
 
@@ -34,5 +78,19 @@ const styles = StyleSheet.create({
       padding: 10,
       flex: 1,
       gap: 10,
+    },
+    ordersStatus: {
+        flexDirection: 'row', 
+        gap: 5
+    },
+    orderStatus: {
+        borderColor: Colors.light.tint,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 5,
+        marginVertical: 10
+    },
+    orderStatusText: {
+        fontSize: 10,
     }
 })
