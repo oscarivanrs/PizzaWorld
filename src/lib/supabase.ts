75af067fe2df6f24e134543618235695639555db
oscarivanrs <oscarivan.sanchez@gmail.com>
2024-12-10 21:50:52 +0000

    chore: .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/database.types'
 
-const supabaseUrl = 'https://sjjlctomjhlempmhcunl.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqamxjdG9tamhsZW1wbWhjdW5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyNjY4OTksImV4cCI6MjA0Njg0Mjg5OX0.MhmK-JY5swNid1YXiG_9uGvHNELXYUzHbL_h1UnXWmk'
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || '';
 
 const options = {
   auth: {
